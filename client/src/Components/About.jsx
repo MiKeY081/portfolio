f04a362708e8066e498171129d9451e407ded95e
Mikie <kcmadhu081@gmail.com>
2023-12-22 02:20:55 +0545

    aba banxa nepal

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,7 +1,7 @@
 // AboutMe.js
 
 import React, { useEffect, useState } from "react";
-import { motion} from "framer-motion";
+import { AnimatePresence, motion} from "framer-motion";
 
 const AboutMe = () => {
   return (
@@ -9,9 +9,11 @@ const AboutMe = () => {
       className={`about min-h-screen pt-16 pb-16 px-20 bg-gray-100 rounded-md shadow-md`}
     >
       <h2 className="text-3xl font-semibold mb-16 text-center">About Me</h2>
+      <AnimatePresence>
       <motion.div
         initial={{ opacity: 0, scale: 0, x: -700 }}
-        whileInView={{ opacity: 1, scale: 1, x: 0 }}
+        animate={{ opacity: 1, scale: 1, x: 0 }}
+        exit={{ opacity: 0, scale: 0, x: -700 }}
         transition={{ duration: 0.6 }}
         viewport={{once:true}}
         className="con text-xl"
@@ -42,6 +44,7 @@ const AboutMe = () => {
           offers to make a positive impact on the world!
         </p>
       </motion.div>
+      </AnimatePresence>
     </div>
   );
 };
