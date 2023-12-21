5bda557b3a8b868a8381265a8e3d15312917f5a4
Mikie <kcmadhu081@gmail.com>
2023-12-22 02:16:40 +0545

    xya

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -1,19 +1,21 @@
 // AboutMe.js
 
 import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { motion} from "framer-motion";
 
 const AboutMe = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0, x: -700 }}
-      whileInView={{ opacity: 1, scale: 1, x: 0 }}
-      transition={{ duration: 0.6 }}
-      viewport={{ once: true }}
+    <div
       className={`about min-h-screen pt-16 pb-16 px-20 bg-gray-100 rounded-md shadow-md`}
     >
       <h2 className="text-3xl font-semibold mb-16 text-center">About Me</h2>
-      <div className="con text-xl">
+      <motion.div
+        initial={{ opacity: 0, scale: 0, x: -700 }}
+        whileInView={{ opacity: 1, scale: 1, x: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{once:true}}
+        className="con text-xl"
+      >
         <p className="mb-4 ">
           I was born in Gulmi, Nepal, in a hilly region, and that's where I was
           raised. Currently, I am pursuing my bachelor's degree in Software
@@ -39,8 +41,8 @@ const AboutMe = () => {
           Let's connect and explore the infinite possibilities that technology
           offers to make a positive impact on the world!
         </p>
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
 
