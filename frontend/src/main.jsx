b3a2c6d8b144c86975d3528b9abcadd0037b504e
Mikie <kcmadhu081@gmail.com>
2024-04-09 01:49:20 +0545

    hiiii

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer } from "react-toastify";
