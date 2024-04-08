4cbe868edc2bb7f9c3eaf95f17716234532563fc
Mikie <kcmadhu081@gmail.com>
2024-04-09 01:57:09 +0545

    hiii

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import {
   FaYoutube,
   FaEnvelope,
@@ -8,53 +9,68 @@ import {
 } from "react-icons/fa";
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const handleToAdminLoginPage = (e) => {
+    e.preventDefault();
+    navigate("/admin/login");
+  };
+
   return (
-    <footer className="bg-gray-800 text-white py-4 ">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex justify-center items-center gap-4">
-          <p className="text-sm">Connect with me:</p>
-          <div className="flex space-x-4">
+    <footer className='bg-gray-800 text-white py-4 '>
+      <div className='container mx-auto flex items-center justify-between'>
+        <div className='flex justify-center items-center gap-4'>
+          <p className='text-sm'>Connect with me:</p>
+          <div className='flex space-x-4'>
             <a
-              href="https://www.youtube.com/channel/UCKRVpjLlFBLLXm-QAhDbkSg"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contactIcons bottomLine"
+              href='https://www.youtube.com/channel/UCKRVpjLlFBLLXm-QAhDbkSg'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='contactIcons bottomLine'
             >
               <FaYoutube />
             </a>
             <a
-              href="mailto:your.kcmadhu081@gmail.com"
-              className="contactIcons bottomLine"
+              href='mailto:your.kcmadhu081@gmail.com'
+              className='contactIcons bottomLine'
             >
               <FaEnvelope />
             </a>
             <a
-              href="https://www.facebook.com/Ryuga.081"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contactIcons bottomLine"
+              href='https://www.facebook.com/Ryuga.081'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='contactIcons bottomLine'
             >
               <FaFacebook />
             </a>
             <a
-              href="https://www.instagram.com/madhu_kunwar081"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contactIcons bottomLine"
+              href='https://www.instagram.com/madhu_kunwar081'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='contactIcons bottomLine'
             >
               <FaInstagram />
             </a>
             <a
-              href="https://github.com/MiKeY081"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contactIcons bottomLine"
+              href='https://github.com/MiKeY081'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='contactIcons bottomLine'
             >
               <FaGithub />
             </a>
           </div>
         </div>
-        <p className="text-sm">&copy; 2003 Madhu Kunwar All rights reserved.</p>
+        <p className='text-sm'>
+          <span
+            onClick={(e) => {
+              handleToAdminLoginPage(e);
+            }}
+          >
+            &copy;
+          </span>{" "}
+          2003 Madhu Kunwar All rights reserved.
+        </p>
       </div>
     </footer>
   );
