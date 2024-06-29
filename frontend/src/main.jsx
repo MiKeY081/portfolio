cf1d786d3b82b8ec41c61419339063856b417206
MiKeY081 <145350419+MiKeY081@users.noreply.github.com>
2024-06-29 10:11:25 +0545

    ok

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,10 +8,10 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 //setting up axios baseUrl and credentials
-axios.defaults.baseURL = "https://madhukunwar.com.np";
+axios.defaults.baseURL = "https://portfolio-czaw.vercel.app";
 axios.defaults.withCredentials = true;
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+https: ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ToastContainer />
     <App />
