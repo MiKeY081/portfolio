4175d0ae3a867ace41be4b505624a07b603f72cf
MiKeY081 <145350419+MiKeY081@users.noreply.github.com>
2024-08-01 16:50:06 +0545

    ok

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Main from "../Components/Hero";
 import About from "../Components/About";
 import Projects from "../Components/Projects";
 import Contact from "../Components/Contact";
 import ScrollToTopButton from "../assets/Hammer";
 import Skills from "../Components/Skills";
+import { RingLoader } from "react-spinners";
 
 const Home = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  return loading ? (
+    <div className='grid place-items-center w-screen h-screen'>
+      <RingLoader color='#11ff00' />
+    </div>
+  ) : (
     <div className='scroll-smooth w-screen overflow-hidden'>
       <Main />
       <About />
